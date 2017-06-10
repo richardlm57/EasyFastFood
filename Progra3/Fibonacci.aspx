474d1eef19c6750ca911ba3f67ce5ab5dedee303
richardlm57 <richardlm57@gmail.com>
2017-06-10 09:52:40 -0600

    Fibonacci

---
+++ Progra3/Fibonacci.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/tplPrincipal.master" AutoEventWireup="true" CodeFile="Fibonacci.aspx.cs" Inherits="Fibonacci" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p>
+        Fibonacci:
+        <asp:TextBox ID="TextBoxFibonacci" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Resultado:
+        <asp:Label ID="LabelFibonacci" runat="server"></asp:Label>
+    </p>
+    <p>
+        <asp:Button ID="ButtonFibonacci" runat="server" Text="Calcular" OnClick="ButtonFibonacci_Click" />
+    </p>
+</asp:Content>
+
---
+++ Progra3/Fibonacci.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Fibonacci : System.Web.UI.Page
+{
+
+    int[] fibonacciSec;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void ButtonFibonacci_Click(object sender, EventArgs e)
+    {
+        int n = int.Parse(TextBoxFibonacci.Text);
+        fibonacciSec = new int[n+1];
+        for (int i = 0; i < n + 1; i++) {
+            fibonacciSec[i] = -1;
+        }
+        int resultado = fibonacci(n);
+        LabelFibonacci.Text = "";
+        for (int i = 0; i < fibonacciSec.Length; i++) {
+            LabelFibonacci.Text += fibonacciSec[i].ToString() + " | ";
+        }
+    }
+    
+    protected int fibonacci(int n){
+        if ((n == 0) || (n == 1))
+        {
+            fibonacciSec[n] = n;
+            return n;
+        }
+        int n1,n2;
+        if (fibonacciSec[n - 1] == -1)
+        {
+            n1 = fibonacciSec[n - 1];
+        }
+        else {
+            n1 = fibonacci(n - 1);
+        }
+        if (fibonacciSec[n - 2] == -1)
+        {
+            n2 = fibonacciSec[n - 2];
+        }
+        else
+        {
+            n2 = fibonacci(n - 2);
+        }
+        fibonacciSec[n]=n1+n2;
+        return n1+n2;
+    }
+
+}
